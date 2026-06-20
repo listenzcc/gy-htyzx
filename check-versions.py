3f8eca376f7b5c7b0e7a71e1b604786994caeb18
listenzcc <cc.public@outlook.com>
2026-06-20 10:12:55 +0800

    Add check-versions script and requirements file for dependency management

---
+++ check-versions.py
@@ -0,0 +1,27 @@
+import fastapi
+import loguru
+import nicegui
+import omegaconf
+import pandas
+import sqlalchemy
+import starlette
+import werkzeug
+
+
+lst = ['# python==3.11.15']
+for e in [
+    fastapi,
+    loguru,
+    nicegui,
+    omegaconf,
+    pandas,
+    sqlalchemy,
+    starlette,
+    werkzeug
+]:
+    lst.append(f'{e.__name__}=={e.__version__}')
+
+print(lst)
+
+with open('./requirements.txt', 'w') as f:
+    f.writelines([e+'\n' for e in lst])
