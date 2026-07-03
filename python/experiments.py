f5db7a33d7ca56b20b6363cbab0e2d77b6e529e4
listenzcc <cc.public@outlook.com>
2026-07-03 17:45:02 +0800

    Enhance experiment handling: update experiment data structure to include abstract and options, and modify run_experiment function to accept input options.

--- python/experiments.py
+++ python/experiments.py
@@ -1,38 +1,53 @@
 from pathlib import Path
+from collections import defaultdict
 
 folders = [
     './experiments/script',
     './experiments/script/模拟任务',
+    './experiments/script/模拟任务1',
     './experiments/script/认知能力测试',
     './experiments/script/认知训练',
 ]
 
-experiments = [
-    # {'script': './experiments/script/单音节分辨.py'},
-    # {'script': './experiments/script/模拟任务/单音节分辨_信号灯2.py'}
-]
+experiments = []
 
 for folder in folders:
     for file in Path(folder).glob('*.py'):
-        experiments.append(
-            {'script': file.as_posix(),
-             'folder': folder,
-             'file': file.name,
-             'cn': file.stem,
-             'abstract': open(file, encoding='utf-8').read().split('"""')[1].strip()
-             })
+        header = open(file, encoding='utf-8').read().split('"""')[1]
+        items = header.split('@')[1:]
+
+        fields = {'type': ['一般任务']}
+        for item in items:
+            if item.lower().startswith('abstract:'):
+                fields['abstract'] = item.strip()
+            if item.lower().startswith('type:'):
+                fields['type'] = [e.strip()
+                                  for e in item[len('type:'):].strip().split() if e.strip()]
+            if item.lower().startswith('parameter:'):
+                fields['options'] = [e.strip()
+                                     for e in item[len('parameter:'):].split('\n') if e.strip()]
+
+        fields.update({
+            'script': file.as_posix(),
+            'folder': folder,
+            'file': file.name,
+            'cn': file.stem,
+        })
+
+        experiments.append(fields)
 
 
 class Experiments:
     experiments = experiments
 
     def __init__(self):
-        # self.load_experiments()
+        self.type_dct = self.detect_types()
         pass
 
-    # def load_experiments(self):
-    #     for exp in self.experiments:
-    #         exp['cn'] = Path(exp['script']).stem
-    #         exp['abstract'] = open(
-    #             exp['script'], encoding='utf-8').read().split('"""')[1].strip()
-    #     pass
+    def detect_types(self):
+        dct = defaultdict(set)
+        for exp in self.experiments:
+            types = exp['type']
+            dct[types[0]].add('.')
+            [dct[types[0]].add(e) for e in types[1:]]
+        return dct
--- python/page_components/experiments_page.py
+++ python/page_components/experiments_page.py
@@ -21,7 +21,16 @@ EXP = Experiments()
 #         abstract='abstract')
 
 
-def run_experiment(dct, output_dir):
+def run_experiment(dct, output_dir, input_options):
+    kwargs = {e: v.value for e, v in input_options.items()}
+    commands = ['conda', 'run', '-n', CONDA_ENV,
+                'python', dct['script'],
+                '--path', output_dir.as_posix()]
+    for e, v in kwargs.items():
+        if v:
+            commands.extend([f'--{e}', v])
+    print(commands)
+    return
     subprocess.run(['conda', 'run', '-n', CONDA_ENV, 'python',
                    dct['script'], '--path', output_dir.as_posix()])
     # import time
@@ -29,7 +38,7 @@ def run_experiment(dct, output_dir):
     return
 
 
-async def start_experiment(event: events.ClickEventArguments, dct: dict, uuid: str = 'uuid'):
+async def start_experiment(event: events.ClickEventArguments, dct: dict, uuid: str = 'uuid', input_options=None):
     btn = event.sender
 
     try:
@@ -42,7 +51,7 @@ async def start_experiment(event: events.ClickEventArguments, dct: dict, uuid: s
             f'Start experiment: {dct=}, {output_dir=}', **NOTIFY_KWARGS.positive)
 
         # Run blocking code in thread
-        await asyncio.to_thread(run_experiment, dct, output_dir)
+        await asyncio.to_thread(run_experiment, dct, output_dir, input_options)
 
     finally:
         btn.enable()
@@ -55,19 +64,70 @@ def experiments_gallery(id: int, uuid: str, user_service: UserService):
     if not user_service.get_user_by_id(id).has_permission('perform_experiment1'):
         ui.label('Permission deny').classes(STYLES.errorText)
 
+    from collections import defaultdict
     exps = {}
+    type_field = {}
+    input_options = defaultdict(dict)
+
     # with ui.card().classes(STYLES.fullCard):
+    row_styles = 'w-full gap-4 justify-center items-start'
     with ui.row().classes('w-full gap-2 justify-center'):
+        for type_name in EXP.type_dct.keys():
+            with ui.expansion(type_name).classes('w-full gap-2 justify-center'):
+                type_field[type_name] = ui.row().classes(row_styles)
+
         # ----------------------------------------------------------------------
         for exp in EXP.experiments:
             exps[exp['script']] = exp
-            card = ui.card().classes(STYLES.column4Card)
-            with card:
-                ui.label(exp['cn']).classes(STYLES.cardTitleLabel)
-                ui.label(exp['folder'])
-                ui.textarea(value=exp['abstract']).classes(
-                    'w-full').props('readonly')
-                ui.button(
-                    'Launch', on_click=lambda e, exp=exp: start_experiment(e, exp, uuid))
+            types = exp.get('type')
+            options = exp.get('options', [])
+
+            with type_field[types[0]]:
+                card = ui.card().classes(STYLES.column4Card)
+                with card:
+                    ui.label(exp['cn']).classes(STYLES.cardTitleLabel)
+                    ui.label(exp['folder'])
+                    ui.label('/'.join(types))
+                    ui.textarea(value=exp.get('abstract', '--')).classes(
+                        'w-full').props('readonly')
+
+                    use_default_value = True
+                    if options:
+                        with ui.expansion('实验选项').classes('w-full'):
+                            for opt in options:
+                                if opt.startswith('#'):
+                                    use_default_value = False
+
+                                if not opt.startswith('-'):
+                                    ui.label(opt)
+                                    continue
+
+                                opt = opt.replace(
+                                    '-', '').replace(',', ' ').replace(':', ' ').strip()
+                                opts = [e.strip()
+                                        for e in opt.split() if e.strip()]
+
+                                n = opts[0]
+                                t = opts[1]
+                                if t == 'int':
+                                    values = [int(opts[2]), int(opts[3])]
+                                    with ui.row().classes('w-full'):
+                                        input_options[exp['script']][n] = ui.number(
+                                            label=f'{n}({t})', min=values[0], max=values[1], step=1, value=values[0] if use_default_value else None).classes('w-full')
+                                elif t == 'float':
+                                    values = [float(opts[2]), float(opts[3])]
+                                    step = (values[1] - values[0]) / 100
+                                    with ui.row().classes('w-full'):
+                                        input_options[exp['script']][n] = ui.number(
+                                            label=f'{n}({t})', min=values[0], max=values[1], step=step, value=values[0] if use_default_value else None).classes('w-full')
+                                elif t == 'option':
+                                    values = opts[2:]
+                                    input_options[exp['script']][n] = ui.select(
+                                        options=values, label=f'{n}({t})', value=values[0] if use_default_value else None).classes('w-full')
+                                else:
+                                    ui.label(opt).classes(STYLES.errorText)
+
+                    ui.button(
+                        'Launch', on_click=lambda e, exp=exp: start_experiment(e, exp, uuid, input_options[exp['script']]))
 
     return
